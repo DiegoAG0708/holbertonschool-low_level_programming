8567ef2fdcc62ca0a1a75308727b8c402685c340
root <root@ip-10-42-67-21.ec2.internal>
2025-10-13 17:12:00 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+  /**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
