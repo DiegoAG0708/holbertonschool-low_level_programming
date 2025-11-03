cac14321f4772f9f0e803032a4ba23bbbf4399f7
root <root@ip-10-42-24-124.ec2.internal>
2025-11-03 15:36:48 +0000

    Task 1: duplicate string with malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,14 +1,13 @@
 #include <stdlib.h>
 
 /**
-* _strdup - memory pointer
+* _strdup - returns a pointer to a newly allocated space in memory,
+*           which contains a copy of the string given as a parameter
+* @str: the string to duplicate
 *
-* @str: the sring to duplicate
-* Return: pointer to the duplicated string
+* Return: pointer to the duplicated string, or NULL if str is NULL or malloc fails
 */
-
 char *_strdup(char *str)
-
 {
 char *copy;
 unsigned int i, length = 0;
@@ -24,8 +23,9 @@ if (copy == NULL)
 return (NULL);
 
 for (i = 0; i < length; i++)
+copy[i] = str[i];
+
 copy[length] = '\0';
 
 return (copy);
-
 }
