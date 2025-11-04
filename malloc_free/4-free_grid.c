ec2b4bd210a43ab48b36e810b5d63c54757c5085
root <root@ip-10-42-46-7.ec2.internal>
2025-11-04 13:52:21 +0000

    Task 4: free 2D grid created with malloc

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+* free_grid - frees a 2D grid created by alloc_grid
+* @grid: pointer to the grid
+* @height: number of rows in the grid
+*
+* Return: Nothing
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+free(grid[i]);
+
+free(grid);
+}
