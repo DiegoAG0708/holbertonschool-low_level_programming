0e50b8609f303604511b911668109f195cac7711
root <root@ip-10-42-53-7.ec2.internal>
2025-11-05 14:05:13 +0000

    Task 0: define struct dog with name, age, owner

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structure to store dog information
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+* Description: This structure holds basic information about a dog.
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
