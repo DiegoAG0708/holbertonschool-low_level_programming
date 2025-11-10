8b64d4b10e4f3c1e64e7b8ef6e44e0996e6d2744
root <root@ip-10-42-255-124.ec2.internal>
2025-11-10 15:51:35 +0000

    Task 2: int_index searches array using comparison function

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+*int_index - Searches for an integer in an array
+*@array: The array of integers
+*@size: The number of elements in the array
+*@cmp: Pointer to the comparison function
+*
+*Return: Index of the first matching element
+*
+*Description: This function loops through the array
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (array == NULL || cmp == NULL || size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+
+return (-1);
+}
