d9555578f699e4b1fa3a95e397d7e3e65e99015a
root <root@ip-10-42-255-124.ec2.internal>
2025-11-10 15:34:26 +0000

    Task 1: array_iterator applies function to each array element

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+*array_iterator - Executes a function on each element of an array
+*@array: The array of integers
+*@size: The number of elements in the array
+*@action: Pointer to the function to apply to each element
+*
+*Description: This function loops through the array and applies
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+size_t i;
+
+if (array == NULL || action == NULL)
+return;
+
+for (i = 0; i < size; i++)
+action(array[i]);
+
+}
