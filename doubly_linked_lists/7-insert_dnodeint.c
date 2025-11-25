de1ce175abbbb65b0f36afcd87df09394220ac64
root <root@ip-10-42-28-95.ec2.internal>
2025-11-25 16:35:27 +0000

    doubly_linked_lists: Task 7 insert_dnodeint_at_index implementation, flush-left style

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+*insert_dnodeint_at_index - Inserts a new node at a given position
+*@h: Address of the pointer to the head node
+*@idx: Index where the new node should be added
+*@n: Integer value for the new node
+*
+*Return: Address of the new element, or NULL on failure
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+dlistint_t *new_node, *tmp;
+unsigned int i = 0;
+
+if (h == NULL)
+return (NULL);
+
+if (idx == 0)
+return (add_dnodeint(h, n));
+
+tmp = *h;
+while (tmp != NULL && i < idx - 1)
+{
+tmp = tmp->next;
+i++;
+}
+
+if (tmp == NULL)
+return (NULL);
+
+if (tmp->next == NULL)
+return (add_dnodeint_end(h, n));
+
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = n;
+new_node->next = tmp->next;
+new_node->prev = tmp;
+
+tmp->next->prev = new_node;
+tmp->next = new_node;
+
+return (new_node);
+}
