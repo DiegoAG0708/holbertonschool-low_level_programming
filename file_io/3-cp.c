75166639206f97746491606bb6398eb12c7c1705
root <root@ip-10-42-84-160.ec2.internal>
2025-11-26 15:52:08 +0000

    Task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,86 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFSIZE 1024
+
+/**
+* safe_close - closes a file descriptor and handles error
+* @fd: file descriptor
+*
+* Return: void
+*/
+static void safe_close(int fd)
+{
+if (close(fd) == -1)
+{
+dprintf(2, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+}
+
+/**
+* main - copies content of file_from to file_to
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0 on success
+*/
+int main(int argc, char **argv)
+{
+int fd_from, fd_to;
+ssize_t r, w;
+char buffer[BUFSIZE];
+
+if (argc != 3)
+{
+dprintf(2, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+dprintf(2, "Error: Can't write to %s\n", argv[2]);
+safe_close(fd_from);
+exit(99);
+}
+
+while ((r = read(fd_from, buffer, BUFSIZE)) > 0)
+{
+ssize_t total = 0;
+while (total < r)
+{
+w = write(fd_to, buffer + total, r - total);
+if (w == -1)
+{
+dprintf(2, "Error: Can't write to %s\n", argv[2]);
+safe_close(fd_from);
+safe_close(fd_to);
+exit(99);
+}
+total += w;
+}
+}
+
+if (r == -1)
+{
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+safe_close(fd_from);
+safe_close(fd_to);
+exit(98);
+}
+
+safe_close(fd_from);
+safe_close(fd_to);
+return (0);
+}
