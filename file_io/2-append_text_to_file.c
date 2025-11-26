5550994caef34c9019e5c1e3a22c54acd695ee2b
root <root@ip-10-42-84-160.ec2.internal>
2025-11-26 15:31:27 +0000

    file_io: task 2 append_text_to_file (append only, no create, correct return semantics)

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+*append_text_to_file - Appends text at the end of a file
+*@filename: Name of the file
+*@text_content: NULL-terminated string to append
+*
+*Return: 1 on success, -1 on failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t w;
+size_t len = 0;
+
+if (filename == NULL)
+return (-1);
+
+/* Open for write, append; do not create if missing */
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+return (-1);
+
+/* If text_content is NULL, do not add anything; success if file opened */
+if (text_content == NULL)
+{
+if (close(fd) == -1)
+return (-1);
+return (1);
+}
+
+/* Compute length of text_content */
+while (text_content[len] != '\0')
+len++;
+
+/* If empty string, nothing to write; this is still success */
+if (len == 0)
+{
+if (close(fd) == -1)
+return (-1);
+return (1);
+}
+
+/* Write all bytes; fail if short write or error */
+w = write(fd, text_content, len);
+if (w == -1 || (size_t)w != len)
+{
+close(fd);
+return (-1);
+}
+
+if (close(fd) == -1)
+return (-1);
+
+return (1);
+}
