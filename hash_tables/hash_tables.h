181d2829ae7f1b4e9a2da18e2effcb8c5cf283e2
root <root@ip-10-42-207-175.ec2.internal>
2025-12-01 17:15:29 +0000

    Add hash_table_create function and header

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,7 +2,14 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key, string
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
 typedef struct hash_node_s
 {
 char *key;
@@ -10,6 +17,11 @@ char *value;
 struct hash_node_s *next;
 } hash_node_t;
 
+/**
+ * struct hash_table_s - Hash table data structure
+ * @size: The size of the array
+ * @array: An array of size @size
+ */
 typedef struct hash_table_s
 {
 unsigned long int size;
@@ -18,4 +30,4 @@ hash_node_t **array;
 
 hash_table_t *hash_table_create(unsigned long int size);
 
-#endif
+#endif /* HASH_TABLES_H */
